b6a39cd29cc3a22725b55d1ac87d2ff0a12f6d7f
Jeongyong Joo <jeongyong95@gmail.com>
2021-01-31 23:41:17 +0900

    스터디 4일차 정리 1/3
    빌트인 객체

---
+++ 4일차/study.js
@@ -0,0 +1,21 @@
+const funny = function () {
+    console.log('자바스크립트 꿀잼!');
+}
+
+function printFunny2() {
+    console.log('자바스크립트 꿀잼!');
+}
+
+funny();
+printFunny();
+printFunny2();
+
+// var, const, let --> 변수
+// 자바스크립트 엔진 실행 : 소스코드 평가 -> 소스코드 실행(런타임)
+//                    선언된 것 파악 -> 할당, 호출
+
+/*
+var printFunny2 = function printFunny2() {
+                        console.log('자바스크립트 꿀잼!');
+                    }
+*/
